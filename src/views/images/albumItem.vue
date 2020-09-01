3e31f11b200ef704283d03d8210b0cc04bc49f98
HuDaDa0 <1497154694@qq.com>
2020-09-01 20:07:21 +0800

    完成相册的编辑和删除

---
+++ src/views/images/albumItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <li class="list-group-item list-group-item-action d-flex align-items-center" 
+      style="justify-content: space-between;"
+      :class="active? 'sum-active active': ''"
+      @click="changeAlbums(index)"
+  >
+    {{ item.name }}
+    <el-dropdown>
+      <span class="btn btn-light btn-sm border">
+        {{ item.num }} <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item @click.stop.native="$emit('edit', {item, index})">修改</el-dropdown-item>
+        <el-dropdown-item  @click.stop.native="$emit('del', index)">删除</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </li>
+  
+</template>
+
+<script>
+export default {
+  name: 'albumItem',
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    },
+    index: {
+      type: Number,
+      default: 0
+    },
+    active: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    changeAlbums(index) {
+      this.$emit('change', index)
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/images/index.vue
+++ src/views/images/index.vue
@@ -14,28 +14,22 @@
             <el-input v-model="value" size="medium" placeholder="请输入内容" style="width: 150px;" class="mr-2"></el-input>
             <el-button type="success" size="medium">搜索</el-button>
         </div>
-        <el-button type="success" size="medium">创建相册</el-button>
+        <el-button type="success" size="medium" @click="openAlbumDialog(false)">创建相册</el-button>
         <el-button type="warning" size="medium">上传图片</el-button>
       </el-header>
       <el-container>
         <el-aside class="bg-white border-right" width="200px" style="position: absolute; top: 60px; left: 0; bottom: 60px;">
           <ul class="list-group list-group-flush">
-            <li class="list-group-item list-group-item-action d-flex align-items-center" style="justify-content: space-between;"
-                v-for="(item, index) of albums" :key="index"
-                :class="albumIndex == index? 'sum-active active': ''"
-                @click="changeAlbums(index)"
-            >
-              {{ item.name }}
-              <el-dropdown>
-                <span class="btn btn-light btn-sm border">
-                  {{ item.num }} <i class="el-icon-arrow-down el-icon--right"></i>
-                </span>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>修改</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </li>
+            <album-item 
+              v-for="(item, index) of albums" 
+              :key="index"
+              :index="index"
+              :item="item"
+              :active="albumIndex == index"
+              @change="changeAlbums"
+              @edit="openAlbumDialog"
+              @del="albumDelete"
+            ></album-item>
           </ul>
         </el-aside>
         <el-main style="position: absolute; top: 60px; left: 200px; right: 0; bottom: 60px;">
@@ -43,13 +37,32 @@
         </el-main>
       </el-container>
       <el-footer>Footer</el-footer>
+
+      <el-dialog title="编辑相册" :visible.sync="dialogAlbumVisible">
+        <el-form :model="albumForm" label-width="100px">
+          <el-form-item label="相册名称">
+            <el-input v-model="albumForm.name" placeholder="亲输入相册名称" style="width: 300px;"></el-input>
+          </el-form-item>
+          <el-form-item label="相册排序">
+            <el-input-number v-model="albumForm.order" :min="0"></el-input-number>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogAlbumVisible=false">取消</el-button>
+          <el-button type="success" @click="confirAlbum">确认</el-button>
+        </div>
+      </el-dialog>
     </el-container>
   </div>
 </template>
 
 <script>
+import albumItem from './albumItem.vue';
 export default {
   name: 'Images',
+  components: {
+    albumItem
+  },
   data() {
     return {
       options: [{
@@ -70,6 +83,8 @@ export default {
         }],
         value: '',
         albumIndex: 0,
+        albumEditIndex: -1,
+        dialogAlbumVisible: false,
         albums: [
           { name: '相册1', num: 4 },
           { name: '相册1', num: 4 },
@@ -81,12 +96,71 @@ export default {
           { name: '相册1', num: 4 },
           { name: '相册1', num: 4 },
           { name: '相册1', num: 4 },
-        ]
+        ],
+        albumForm: {
+          name: '',
+          order: ''
+        }
     }
   },
   methods: {
     changeAlbums(index) {
       this.albumIndex = index
+    },
+    openAlbumDialog(obj) {
+      // obj 存在就是编辑
+      if(obj) {
+        let { item, index } = obj;
+        this.albumForm.name = item.name;
+        this.albumForm.order = item.order;
+        this.albumEditIndex = index;
+
+        return this.dialogAlbumVisible = true
+      }
+      // 创建
+      this.albumForm = {
+        name: '',
+        order: 0
+      }
+      this.albumEditIndex = -1
+      this.dialogAlbumVisible = true
+    },
+    confirAlbum() {
+      // 编辑
+      if (this.albumEditIndex > -1) {
+        this.albums[this.albumEditIndex].name = this.albumForm.name;
+        this.albums[this.albumEditIndex].order = this.albumForm.order;
+        this.dialogAlbumVisible = false
+        this.$message({
+          type: 'success',
+          message: '编辑成功'
+        })
+        return false
+      } else {
+        this.albums.unshift({
+          name: this.albumForm.name,
+          order: this.albumForm.order,
+          num: 0
+        })
+        this.dialogAlbumVisible = false
+        this.$message({
+          type: 'success',
+          message: '创建成功'
+        })
+      }
+    },
+    albumDelete(index) {
+      this.$confirm('是否删除该相册', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'danger'
+      }).then(() => {
+        this.albums.splice(index, 1)
+        this.$message({
+          type: 'success',
+          message: '删除成功'
+        })
+      })
     }
   }
 }
